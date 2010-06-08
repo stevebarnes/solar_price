3cb347b37070c377fb2dca52ee2bd319e7d9f2c4
Steve Barnes <steve@busigeeks.com.au>
2010-06-08 13:14:17 +1000

    bug fix: postcodes array stored against supplier

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,4 +1,4 @@
 class Supplier < ActiveRecord::Base
-  attr_accessible :name, :first_name, :last_name, :abn, :phone, :email, :suburb, :state, :country, :active
+  attr_accessible :name, :first_name, :last_name, :abn, :phone, :email, :suburb, :state, :country, :active, :postcodes_requested
   has_many :supplier_areas
 end
