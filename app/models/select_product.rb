00e5d43fba98bb76e3bb86ce73649ba465e89247
Steve Barnes <steve@busigeeks.com.au>
2010-06-17 17:02:29 +1000

    site_details : form now validated except for radio buttons, which I need advice on

--- app/models/select_product.rb
+++ app/models/select_product.rb
@@ -1,4 +1,21 @@
 class SelectProduct < ActiveRecord::Base
   attr_accessible :product, :timeframe
   belongs_to :postcode
+
+  validate :check_fields
+  
+  def check_fields
+    # following statement does not work
+    #
+    # You have a nil object when you didn't expect it!
+    #You might have expected an instance of Array.
+    #The error occurred while evaluating nil.empty?
+    #
+    #RAILS_ROOT: /home/steve/RailsProjects/solarprice_nifty
+    #Application Trace | Framework Trace | Full Trace
+    #
+    #/home/steve/RailsProjects/solarprice_nifty/app/models/select_product.rb:9:in `check_fields'
+
+#    errors.add_to_base("Please supply an interest") if self.product.empty?
+  end
 end
--- app/models/site_details.rb
+++ app/models/site_details.rb
@@ -2,5 +2,13 @@ class SiteDetails < ActiveRecord::Base
   attr_accessible :quote_type, :first_name, :last_name, :email, :street_address, :suburb, :state, :postcode, :country
   belongs_to :postcode
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
-  
+
+  validate :check_fields
+
+  def check_fields
+    errors.add_to_base("Please supply your first name") if self.first_name.empty?
+    errors.add_to_base("Please supply your last name") if self.last_name.empty?
+    errors.add_to_base("Please supply your address") if self.street_address.empty?
+    errors.add_to_base("Please supply your suburb") if self.suburb.empty?
+  end
 end
