143c41624c5d8ce2c5d65b31da08112b32c69ed9
Steve Barnes <steve@busigeeks.com.au>
2010-06-17 11:10:40 +1000

    we have now split the requested postcodes into the supplier_area table

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,15 +2,15 @@ class Supplier < ActiveRecord::Base
   attr_accessible :name, :first_name, :last_name, :abn, :phone, :email, :suburb, :state, :country, :active, :postcodes_requested
   has_many :supplier_areas
 
-#  after_save :store_postcodes
-#
-#  def store_postcodes
-#    supp = self
-#    a = self.postcodes_requested.split(",")
-#    a.each do |x|
-#      supplier_areas.new
-#      :postcode == x
-#      supplier_areas.save
-#    end
-#  end
+  after_save :store_postcodes
+
+  def store_postcodes
+    @requested.split(",").each do |x|
+      supplier_areas.create(:postcode=>x)
+    end
+  end
+
+  def postcodes_requested=(requested)
+    @requested = requested
+  end
 end
