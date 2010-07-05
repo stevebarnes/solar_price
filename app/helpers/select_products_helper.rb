73453d78872d89791b7a20360d41c332f2f03368
Steve Barnes <steve@busigeeks.com.au>
2010-07-05 18:25:48 +1000

    end of day commit

--- app/helpers/select_products_helper.rb
+++ app/helpers/select_products_helper.rb
@@ -1,2 +1,22 @@
 module SelectProductsHelper
+
+  def display_timeframe(tf)
+    if tf == "0"
+      tfr = "immediately"
+    end
+    if tf ==  "1"
+      tfr = "in the next month"
+    end
+    if tf == "6"
+      tfr = "in the next six months"
+    end
+    if tf == "12"
+      tfr = "in the next year"
+    end
+    if tf == "99"
+      tfr = "Just looking, thanks"
+    end
+    return tfr
+  end
+
 end
